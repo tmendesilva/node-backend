389fe9ed8ee92f7b2c8b41fbd777a6217eea19f4
Tiago Mendes <tiago@aptas.com.br>
2019-07-07 13:29:10 -0300

    Fix on server.js

--- src/server.js
+++ src/server.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(cors());
 
 const server = require('http').Server(app);
-const io = require('server.io')(server);
+const io = require('socket.io')(server);
 
 io.on('connection', socket => {
     console.log('ok');
@@ -31,7 +31,6 @@ app.use(express.urlencoded({ extended: true }));
 app.use('/files', express.static(path.resolve(__dirname, '..', 'tmp')));
 
 app.use(require('./routes'));
-
 
 app.listen(process.env.PORT || 3333);
 
