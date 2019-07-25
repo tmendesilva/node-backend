5a33f7e4b66ed0f1165e6517bb3c165222e94976
Tiago Mendes <tiago@aptas.com.br>
2019-07-25 20:37:03 -0300

    fix

--- src/server.js
+++ src/server.js
@@ -5,10 +5,8 @@ const socketIO = require('socket.io');
 
 const PORT = process.env.PORT || 3333;
 const INDEX = path.join(__dirname, 'index.html');
-const server = express()
-  .use((req, res) => res.sendFile(INDEX) )
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`));
-
+const server = express();
+server.use((req, res) => res.sendFile(INDEX) ).listen(PORT, () => console.log(`Listening on ${ PORT }`));
 
 const io = socketIO(server);
 io.on('connection', (socket) => {
