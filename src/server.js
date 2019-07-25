5c547f5f8a6c2f8f19633fd5a44f76b6fae9a356
Tiago Mendes <tiago@aptas.com.br>
2019-07-25 20:16:36 -0300

    fix

--- src/server.js
+++ src/server.js
@@ -1,35 +1,27 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const path = require('path');
-const cors = require('cors');
+const socketIO = require('socket.io');
 
-const app = express();
+const PORT = process.env.PORT || 3333;
+const INDEX = path.join(__dirname, 'index.html');
+const server = express()
+  .use((req, res) => res.sendFile(INDEX) )
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`));
 
-const server = require('http').createServer(app);
-const io = require('socket.io').listen(server);
-io.origins("*:*");
 
-io.on('connection', socket => {
-    socket.on('connectRoom', box => {
-        socket.join(box);
-    })
+const io = socketIO(server);
+io.on('connection', (socket) => {
+  socket.on('connectRoom', box => {
+      socket.join(box);
+  });
 });
 
 mongoose.connect('mongodb+srv://react:react@cluster0-jl4i1.mongodb.net/react?retryWrites=true&w=majority', {
     useNewUrlParser: true
 });
 
-app.use(cors());
-app.use((req, res, next) => {
-    req.io = io;
-    return next();
-});
-
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use('/files', express.static(path.resolve(__dirname, '..', 'tmp')));
-
-app.use(require('./routes'));
-
-app.listen(process.env.PORT || 3333);
-
+server.use(express.json());
+server.use(express.urlencoded({ extended: true }));
+server.use('/files', express.static(path.resolve(__dirname, '..', 'tmp')));
+server.use(require('./routes'));
